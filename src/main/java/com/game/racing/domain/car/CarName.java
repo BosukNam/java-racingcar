02969905687ac03bc6cdd9f0f128b1ef09f9f206
BosukNam <popman1105@gmail.com>
2022-10-04 22:09:02 +0900

    추가 피드백 반영
    - validation 로직 method 화

--- src/main/java/com/game/racing/domain/car/CarName.java
+++ src/main/java/com/game/racing/domain/car/CarName.java
@@ -6,13 +6,21 @@ public class CarName {
     private final String carName;
 
     public CarName(String carName) {
-        if (carName == null || carName.isBlank()) {
-            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다!");
-        }
+        isValidCarName(carName);
+        isValidLength(carName);
+        this.carName = carName;
+    }
+
+    private void isValidLength(String carName) {
         if (carName.length() > MAX_LENGTH_OF_CAR_NAME) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다!");
         }
-        this.carName = carName;
+    }
+
+    private void isValidCarName(String carName) {
+        if (carName == null || carName.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다!");
+        }
     }
 
     public String getCarName() {
